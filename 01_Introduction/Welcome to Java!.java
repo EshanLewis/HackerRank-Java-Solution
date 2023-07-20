852967204cc943f19ffbdbfb0ed6abf6f9e9b8cf
Eshan <140048486+EshanLewis@users.noreply.github.com>
2023-07-20 12:19:04 +0530

    Add files via upload
    Welcome to Java!

---
+++ 01_Introduction/Welcome to Java!.java
@@ -0,0 +1,13 @@
+/*Author: Eshan
+**URI: https://github.com/EshanLewis/
+*/
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
